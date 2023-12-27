9adc2cd0ea41578f793e48fca1d3bcad64c9e269
Yakuson2021 <91000630+Yakuson2021@users.noreply.github.com>
2023-12-27 23:21:29 +0900

    Update associative-array.php

--- kadai_007/associative-array.php
+++ kadai_007/associative-array.php
@@ -7,7 +7,7 @@
  <body>
      <p>
          <?php
-         $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+         $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
          print_r($personal_data)
          ?>
      </p>
